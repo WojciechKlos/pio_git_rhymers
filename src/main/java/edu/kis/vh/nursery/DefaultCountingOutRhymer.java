785ff87d8395276ae588810fd3d2a77c74edf5aa
Wojciech Kłos <mail>
2025-04-02 20:37:33 +0200

    Zastosowanie się do poleceń z pracy równoległej

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,20 +4,16 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
-        return total == -1;
-    }
+    public boolean callCheck() { return total == -1; }
 
-    public boolean isFull() {
-        return total == 11;
-    }
+    public boolean isFull() { return total == 11; }
 
     protected int peekaboo() {
         if (callCheck())
@@ -30,5 +26,5 @@ public class DefaultCountingOutRhymer {
             return -1;
         return numbers[total--];
     }
-
+    public int getTotal() { return total; }
 }
