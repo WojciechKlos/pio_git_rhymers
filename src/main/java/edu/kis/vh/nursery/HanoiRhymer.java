4b1c0d44fd6977000449c9d8fda494e0e9a86205
brtkx13 <brtkx13@gmail.com>
2025-04-03 04:24:10 +0200

    6-8. Implementacja staÅlych i annotacji

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -6,6 +6,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     public int reportRejected() { return totalRejected; }
 
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
